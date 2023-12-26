fdf2ad501ff419a84a64a58fe896fe85d1ee738a
Karolis92 <19680461+Karolis92@users.noreply.github.com>
2023-12-26 14:38:41 +0200

    Notifee poc

--- App.tsx
+++ App.tsx
@@ -1,15 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import notifee, { EventType } from '@notifee/react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
+  async function displayNotification() {
+    await notifee.requestPermission()
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    await notifee.displayNotification({
+      id: "notification1",
+      title: 'Title',
+      body: 'Content',
+      android: {
+        channelId,
+        ongoing: true,
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+
+    const unsub = notifee.onForegroundEvent(({ type, detail }) => {
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('User dismissed notification', detail.notification?.id);
+          unsub()
+          break;
+      }
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Display Notification" onPress={() => displayNotification()} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
