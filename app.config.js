e85804a352394789801e928a2fade4e3b57b4991
Karolis92 <19680461+Karolis92@users.noreply.github.com>
2024-04-15 20:06:39 +0300

    "Temporary" icons and splashscreen

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ export default {
   splash: {
     image: "./assets/splash.png",
     resizeMode: "contain",
-    backgroundColor: "#ffffff",
+    backgroundColor: "#2B2B2B",
   },
   assetBundlePatterns: ["**/*"],
   ios: {
@@ -19,7 +19,7 @@ export default {
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
-      backgroundColor: "#ffffff",
+      backgroundColor: "#2B2B2B",
     },
     package: APP_VARIANT ? `app.annoytify.${APP_VARIANT}` : "app.annoytify",
   },
